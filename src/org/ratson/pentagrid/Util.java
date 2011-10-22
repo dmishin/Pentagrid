9d0f19bd3dfa8ab1b3b27669552097bbb204c920
Dmishin <shintyakov@gmail.com>
2011-10-23 00:57:37 +0400

    Cell iteration in functional style

--- src/org/ratson/pentagrid/Util.java
+++ src/org/ratson/pentagrid/Util.java
@@ -2,27 +2,38 @@ package org.ratson.pentagrid;
 
 import java.util.LinkedList;
 
+import org.ratson.util.Function1;
+
 /**Some utility functions, I don't know where to put to.*/
 public class Util {
 	/**Create random pentagonal field*/
-	public static Path[] randomField( int radius, double p) {
-		LinkedList<Path> cells = new LinkedList<Path>();
-		Path root = Path.getRoot();
-		for( int r = 0 ; r < radius; ++r ){
-			Path cur = root;
-			do{
-				if (Math.random() <= p ){
-					cells.add( cur );
-				}
-				cur = cur.left();
-			}while( ! cur.equals(root) );
-			root = root.child(1);
-		}
+	public static Path[] randomField( int radius, final double p) {
+		final LinkedList<Path> cells = new LinkedList<Path>();
+
+		forField( radius, new Function1<Path, Boolean>() {
+			public Boolean call(Path path) {
+				if (Math.random() <= p ) cells.add( path );
+				return true;
+			}
+		});
 		
 		//convert result to array
 		Path[] rval = new Path[ cells.size() ];
 		cells.toArray( rval );
 		return rval;
+	}
+	/**Iterate over the field of radius R, in funcitonal style*/
+	public static void forField( int radius, Function1<Path, Boolean>callback){
+		Path root = Path.getRoot();
+		for( int r = 0 ; r < radius; ++r ){
+			Path cur = root;
+			do{
+				if ( ! callback.call( cur ) )
+					return;
+				cur = cur.left();
+			}while( !cur.equals(root) );
+			root = root.child(1);
+		}
 	}
 	/**Given a field and cell, returns new field with one cell toggled*/
 	public static Path[] toggleCell( Path[] cells, Path cell ){
